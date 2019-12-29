a79283d39396017e1c5ba2dc53853fd026bad8a1
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-28 21:38:17 -0700

    fancy credit screen

---
+++ Chapter2/fancy_credits.py
@@ -0,0 +1,20 @@
+# Fancy Credits
+# Demonstrates Escape Sequences
+
+print("\t\t\tFancy Credits")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tMark Ashinhust")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+
+print("\n Special Thanks goes out to:")
+print("My Dog, Dodger \'The Great,\'who never says \"can\'t.\"")
+
+# sound the system bell
+
+print("\a")
+
+input("\n\nPress the ENTER key to exit")
+
