9e0397e212efad2f3e5cad8f7d60f563a85990ca
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-29 16:40:13 -0700

    uses math to explain a word problem

---
+++ Chapter2/word_problem.py
@@ -0,0 +1,11 @@
+# Word Problem
+# Demonstrates Numbers and Math
+
+print("If a 2000 pound pregnant hippo give birth to a 100 pound calf.")
+print("but then eats 50 pounds of food, how much does she weigh?\n")
+input("Press the ENTER key to find out.\n")
+print("2000 - 100 + 50 =", 2000 - 100 + 50) # Here, the numbers are used
+
+input("\n\nPress the ENTER key to exit the program.")
+
+# the '\n' is used to space out the responses
