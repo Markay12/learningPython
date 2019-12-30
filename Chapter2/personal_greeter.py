2598c53a560acdada652d3344a3d06c403530047
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-29 18:13:04 -0700

    same as greeter.py but with input

---
+++ Chapter2/personal_greeter.py
@@ -0,0 +1,11 @@
+# Personal Greeter
+# Demonstrates Gathering User Input
+
+name = input("What is your name?: ")
+
+print(name + "\n")
+
+print("Hi, " + name)
+
+input("\n\nPress ENTER to exit the program.")
+
