96cfd396fc9f7e75a174809f1502bc997b6ad0c4
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-29 18:12:48 -0700

    user input to greet themselves

---
+++ Chapter2/greeter.py
@@ -0,0 +1,13 @@
+# Greeter
+# Understanding Variables and how to use them
+
+
+# variables are used to store information
+
+name = "Mark"
+
+print(name)
+
+print("Hi, " + name)
+
+input("\n\nPress ENTER to exit program.")
