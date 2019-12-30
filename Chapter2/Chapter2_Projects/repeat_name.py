57510de105a183b0dbef0f120332d9f6d5a02429
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-30 15:10:04 -0700

    repeats users name

---
+++ Chapter2/Chapter2_Projects/repeat_name.py
@@ -0,0 +1,8 @@
+# Repeat Name
+# Demonstrates how to repeat a name using the '*' operator
+
+name = input("What is your name?: ")
+
+print("\nIf a little kid was trying to get your attention, they would say: ", name * 5)
+
+input("\nPress ENTER to exit the program.") # Line to keep message printed out
