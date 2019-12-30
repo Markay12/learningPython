7698db1f7dc6f1b81d7c8729b8aad16e9b7aab70
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-30 15:07:48 -0700

    Tells useless information about the user

---
+++ Chapter2/useless_trivia.py
@@ -0,0 +1,22 @@
+# Useless Trivia
+#
+# Gets personal information from the user and prints true unless information about him or her
+
+# Use these beginning comments to show people what you have done to your code and let them know the updates you have made.
+# Many experienced programmers do so to show their updates over time
+
+name = input("Hi. What's your name?: ")
+
+age = (int)(input("How old are you?: "))
+
+weight = int(input("Okay, last question. How many pounds do you weigh?: "))
+
+print("Awesome, here is what I got:\n")
+print("Name:", name)
+print("Age:", age)
+print("Weight:", weight, "\n")
+
+input("Press ENTER to exit the program")
+
+
+
