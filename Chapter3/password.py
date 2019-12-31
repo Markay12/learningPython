9ac691e28aea65488cfa9ed03d7e9688c7056f4d
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-30 20:01:15 -0700

    First use of elif statements

---
+++ Chapter3/password.py
@@ -0,0 +1,17 @@
+# Password
+# Demonstrates the use of an If statement
+
+print("Welcome to System Security Inc...")
+print("-- where security is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("\nAccess Granted")
+    print("You must be someone really important")
+else:
+    print("\nSorry. That is not correct. Leave NOW")
+
+input("\n\nPress ENTER to exit the program.")
+
+
