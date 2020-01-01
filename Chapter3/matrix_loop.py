57a10b23e5474fa2b5d09943cd27a7f68634f696
Markay12 <54555076+Markay12@users.noreply.github.com>
2020-01-01 14:08:59 -0700

    matrix looking random number

---
+++ Chapter3/matrix_loop.py
@@ -0,0 +1,10 @@
+# Matrix Loop
+# Demonstrates the use of random and loops to create a matrix looking program
+
+import random
+
+for matrix in range(1000):
+    print(random.randrange(2))
+
+
+input("\n\nPress ENTER to exit out of the program.")
